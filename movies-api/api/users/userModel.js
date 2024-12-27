58d1d74b41e26f54157bc2372f9a053809a6d2c7
thatgirlonfire18 <116007719+Adam-Costigan-Dooley@users.noreply.github.com>
2024-12-27 12:36:39 +0000

    salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 const passwordValidator = (password) => {
@@ -14,6 +15,28 @@ const UserSchema = new Schema({
           validator: passwordValidator,
           message: props => `${props.value} is not a valid password!`
       }
+  }
+});
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+}
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
   }
 });
 
