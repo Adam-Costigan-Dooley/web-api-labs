df192014436f07db692e9bd04d8b19d9d71c0e97
thatgirlonfire18 <116007719+Adam-Costigan-Dooley@users.noreply.github.com>
2024-12-27 15:49:10 +0000

    Intergration Lab - Intergrate Users Endpoints

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,6 +1,30 @@
+
+
 export const getMovies = async () => {
     const response = await  fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=73e7d239bd6ed72ff2ff64e73ceb81ea&language=en-US&include_adult=false&page=1`
     )
     return response.json()
   };
+
+export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
